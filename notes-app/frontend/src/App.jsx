5af8b9cdc9a8334d773fc43355b47900fe86868c
Tom Mc Carthy (Student) <N00230551@iadt.ie>
2026-09-15 11:35:36 +0100

    Add react frontend

---
+++ notes-app/frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+fetch("/api/notes");
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/notes")
+      .then((res) => res.json())
+      .then((data) => setNotes(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>My notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
